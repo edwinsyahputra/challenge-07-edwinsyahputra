b6abfec62102890266f2b67633ee73155c004454
Edwin Syahputra <96719900+edwinsyahputra@users.noreply.github.com>
2022-06-12 21:30:06 +0700

    Answer challange 7

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (respon) => {
+  return Promise.all([promiseTheaterIXX(), promiseTheaterVGC()])
+    .then((response) => {
+      const [ixx, cgv] = response;
+      return [...ixx, ...cgv].filter((item) => item.hasil === respon).length;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 module.exports = {
   promiseOutput,
